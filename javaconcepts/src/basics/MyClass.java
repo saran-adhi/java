591bf30343e3d921cda38553377e730fd3c5aba3
saranadhi <saranaadithyanv@gmail.com>
2024-03-17 11:52:38 +0530

    addition method created

--- javaconcepts/src/basics/MyClass.java
+++ javaconcepts/src/basics/MyClass.java
@@ -2,9 +2,12 @@ package basics;
 
 public class MyClass {
 
+	private static int add(int a,int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
+		System.out.println("Addition of 2 values ,"+add(5,6));
 		
-			System.out.println("Success");
 	}
 
 }
