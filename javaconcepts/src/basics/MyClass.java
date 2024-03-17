c09a2762028bcc5edca74553eb641a4c8df3939c
saranadhi <saranaadithyanv@gmail.com>
2024-03-17 11:58:30 +0530

    modifying method in math

--- javaconcepts/src/basics/MyClass.java
+++ javaconcepts/src/basics/MyClass.java
@@ -5,8 +5,23 @@ public class MyClass {
 	private static int add(int a,int b) {
 		return a+b;
 	}
+	
+	private static int sub(int a,int b) {
+		return a-b;
+	}
+	
+	private static int multiply(int a,int b) {
+		return a*b;
+	}
+	
+	private static int divide(int a,int b) {
+		return a%b;
+	}
 	public static void main(String[] args) {
-		System.out.println("Addition of 2 values ,"+add(5,6));
+		System.out.println("Addition of 2 values ,"+add(6,5));
+		System.out.println("Subtraction of 2 values ,"+sub(6,5));
+		System.out.println("Multiplication of 2 values ,"+multiply(6,5));
+		System.out.println("Division of 2 values ,"+divide(6,5));
 		
 	}
 
